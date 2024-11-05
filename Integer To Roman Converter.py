3542413ab1399dd768b73027d2d0bbcb5a3b50a6
dostofall <113549462+dostofall@users.noreply.github.com>
2024-11-06 00:00:44 +0530

    Initial commit

--- Integer To Roman Converter.py
+++ Integer To Roman Converter.py
@@ -1,1 +1,18 @@
-print("Hello world")
+#Program to convert integers into roman numerals
+
+integer = int(input("Enter the number to conver it to roman numeral here: "))
+basicromanvalues = {"M":1000,"D":500,"C":100,"L":50,"X":10,"V":5,"I":1}
+repeatableromanvalues = {"M":1000,"C":100,"X":10,"I":1}
+
+basicromanvalueskeys = list(basicromanvalues.keys())
+
+romancharactercounts = {"M":0,"D":0,"C":0,"L":0,"X":0,"V":0,"I":0}
+
+maxcountexceeded = False
+
+romannumeral =""
+
+while integer !=0 or maxcountexceeded == False:
+    if integer == 0:
+        romannumeral = ""
+    
