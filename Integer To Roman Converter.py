6a05dd06c0a97015c28ccff54d9c94aa63cbc242
dostofall <113549462+dostofall@users.noreply.github.com>
2024-11-06 20:16:05 +0530

    partially working code 2

--- Integer To Roman Converter.py
+++ Integer To Roman Converter.py
@@ -3,16 +3,22 @@
 integer = int(input("Enter the number to conver it to roman numeral here: "))
 basicromanvalues = {"M":1000,"D":500,"C":100,"L":50,"X":10,"V":5,"I":1}
 repeatableromanvalues = {"M":1000,"C":100,"X":10,"I":1}
-
+romancharacters = ["M","D","C","L","X","V","I"]
 basicromanvalueskeys = list(basicromanvalues.keys())
+romanvalues = [1000,500,100,50,10,5,1]
 
-romancharactercounts = {"M":0,"D":0,"C":0,"L":0,"X":0,"V":0,"I":0}
-
-maxcountexceeded = True
+basicromanvaluesvalues = list(basicromanvalues.values())
 
 romannumeral = ""
 
-currentexceedingvalues = []
-
 while integer != 0:
     for i in basicromanvalues:
+        if basicromanvalues[i] < integer:
+            integer -= basicromanvalues[i]
+            romannumeral += i
+        elif basicromanvalues[i] - integer in romanvalues:
+            romannumeral = romannumeral + romancharacters[romanvalues.index(basicromanvalues[i] - integer)]
+            romannumeral += i
+            integer -= integer
+            break
+print(romannumeral)
