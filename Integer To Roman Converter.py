db18acddb2c66a1004e29403828f486618cb747f
Sidharth G Kumar <sidharthgkumar73@gmail.com>
2024-11-09 20:55:37 +0530

    Working version: 1-3999

--- Integer To Roman Converter.py
+++ Integer To Roman Converter.py
@@ -1,30 +1,55 @@
 #Program to convert integers into roman numerals
 
 integer = int(input("Enter the number to conver it to roman numeral here: "))
-basicromanvaluecount = {"M":0,"D":0,"C":0,"L":0,"X":0,"V":0,"I":0}
-repeatableromanvalues = ["M","C","X","I"]
 romancharacters = ["M","D","C","L","X","V","I"]
-basicromanvalueskeys = list(basicromanvaluecount.keys())
 romanvalues = [1000,500,100,50,10,5,1]
 
-basicromanvaluesvalues = list(basicromanvaluecount.values())
+romannumeral = []
 
-romannumeral = ""
+multiplier = 1
+parts_of_integer = []
 
 while integer != 0:
-    for i in romanvalues:
-        if i == integer:
-            integer = 0
-            break
-        if i < integer:
-            integer -= i
-            basicromanvaluecount[(romancharacters[romanvalues.index(i)])] += 1
-        
-for i in basicromanvaluecount:
-    if basicromanvaluecount[i] > 3:
-        
+    parts_of_integer.append(multiplier*(integer%10))
+    integer = integer // 10
+    multiplier *= 10
+parts_of_integer.reverse()
 
-        
-        
+for i in parts_of_integer:
+    currentint = i
+    tempromannumeral = ""
+    if i not in romanvalues:
+        while currentint != 0:
+            for k in romanvalues:
+                if k < currentint and (currentint%k != 0 and (k*4 > currentint)):
+                    currentint -= k
+                    tempromannumeral += romancharacters[romanvalues.index(k)]
 
-print(romannumeral)
+                else:
+                    if currentint % k == 0 and currentint//k <= 3:
+                        tempromannumeral += romancharacters[romanvalues.index(k)] * (currentint//k)
+                        currentint = 0
+                        break
+                    elif  ((len(romanvalues)-1)-romanvalues.index(k)) >= 1:
+                        if ((len(romanvalues)-1)-romanvalues.index(k)) >= 1:
+                            if k - (romanvalues[romanvalues.index(k)+1]) == i:
+                                currentint = 0
+                                tempromannumeral = romancharacters[romanvalues.index(k)+1] + romancharacters[romanvalues.index(k)]
+                                break
+                            elif ((len(romanvalues)-1)-romanvalues.index(k)) > 1:
+                                if k - (romanvalues[romanvalues.index(k)+2]) == i:
+                                    currentint = 0
+                                    tempromannumeral = romancharacters[romanvalues.index(k)+2] + romancharacters[romanvalues.index(k)]
+                                    break
+    else:
+        tempromannumeral = romancharacters[romanvalues.index(i)]
+        currentint = 0
+                        
+    romannumeral.append(tempromannumeral)
+            
+
+roman = ""
+for i in romannumeral:
+    roman += i
+
+print(roman)
